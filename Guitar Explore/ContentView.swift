28be5e0a9c3bb3f45d1d056d8971ae074419ce41
Akisanya Temiloluwa <tolutech2004@gmail.com>
2024-02-27 17:31:43 +0100

    Initial Commit

---
+++ Guitar Explore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Guitar Explore
+//
+//  Created by Temiloluwa Akisanya on 27/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Guitar Explore/Guitar_ExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guitar_ExploreApp.swift
+//  Guitar Explore
+//
+//  Created by Temiloluwa Akisanya on 27/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Guitar_ExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
